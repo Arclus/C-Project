4f93ebc7dedee9018ba451b8635cfca8def19657
Arclus <darksun5858@gmail.com>
2017-02-07 17:16:16 +0200

    Add files via upload

---
+++ 0'a En Yakın Mutlak Deger Fonksiyonu/main1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// 0 a en yakyn de?eri bulma
+// fabs mutlak deger fonksiyonu
+
+int main()
+{
+	int temps[10]={9,2,3,4,-2,-6,7,-8,9,10};
+    int pclosest, i, j;
+    
+    pclosest = temps[0];
+    
+        for( i = 0; i < 10; i++ )
+        {
+          	if( fabs(temps[i]) < fabs(pclosest) )
+          	{
+          		pclosest = temps[i];
+			}        		
+        }
+        
+    printf("%d", pclosest);
+    
+    return 0;
+}
